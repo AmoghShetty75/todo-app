7a934129622ca6c3ba2277e3391a2a5fea78f89a
AmoghShetty75 <amoghshetty75@gmail.com>
2024-07-17 16:51:54 +0530

    last commit

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose")
+require('dotenv').config();
 
 
-mongoose.connect('mongodb+srv://amoghc174:wjkAQKKwHUQTgDbY@cluster0.lcozspr.mongodb.net/todos')
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
 const todoSchema = mongoose.Schema({
     title:String,
